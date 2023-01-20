1fe688a3888fb9311c710c4f2e5df8b0347a717e
yewonbahn <banyaewon98@gmail.com>
2023-01-20 11:51:17 +0900

    feat: add code for error

--- 2023-shinhan/shinhanrest/product/serializers.py
+++ 2023-shinhan/shinhanrest/product/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product,Comment
+from .models import Product,Comment,Like
 
 class ProductSerializer(serializers.ModelSerializer):
 
@@ -12,3 +12,34 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        request = self.context['request']
+        if request.user.is_authenticated:
+            attrs['member']=request.user
+        else:
+            raise serializers.ValidationError("member is required")
+
+        print(request.user)
+        return attrs
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        extra_kwargs={'member':{'required':False}}
+
+class LikeCreateSerializer(serializers.ModelSerializer):
+    member = serializers.HiddenField(
+        default = serializers.CurrentUserDefault(),
+        required = False
+    )
+    def validate_member(self, value):
+        if not value.is_authenticated :
+            raise serializers.ValidationError("member is required")
+        return value
+
+    class Meta:
+        model = Like
+        fields = "__all__"
+        # extra_kwargs={'member':{'required':False}}
